9131fd4f11d1f9ca0934f3dabc15c29b742ba0b3
bmilojkovic <bandza88@gmail.com>
2025-11-21 08:09:51 +0100

    Making changes to move from testing to production

--- src/py/send_to_argus.py
+++ src/py/send_to_argus.py
@@ -7,13 +7,24 @@ import argparse
 import argus_auth
 from argus_util import argus_log
 
-#argus_backend = "http://localhost:3000"
-#argus_backend = "https://argus-h2-backend.fly.dev"
-argus_backend = "https://argus-h2-backend-test.fly.dev"
+# argus_backend = "http://localhost:3000"
+argus_backend = "https://argus-h2-backend.fly.dev"
+# argus_backend = "https://argus-h2-backend-test.fly.dev"
 
-async def send_to_backend(pluginpath, boon_data, weapon_data, familiar_data, extra_data, elemental_data, pin_data, vow_data, arcana_data):
+
+async def send_to_backend(
+    pluginpath,
+    boon_data,
+    weapon_data,
+    familiar_data,
+    extra_data,
+    elemental_data,
+    pin_data,
+    vow_data,
+    arcana_data,
+):
     argus_token = argus_auth.get_argus_token(pluginpath, argus_backend)
-    
+
     if argus_token == "FAIL":
         argus_log("Failed to read token from config file. :(")
         return
@@ -28,24 +39,26 @@ async def send_to_backend(pluginpath, boon_data, weapon_data, familiar_data, ext
     argus_log("Pins: " + str(pin_data.strip()))
     argus_log("Vows: " + str(vow_data.strip()))
     argus_log("Arcana: " + str(arcana_data.strip()))
-   
+
     response = requests.post(
         argus_backend + "/run_info",
-        json = {
+        json={
             "argusProtocolVersion": "2",
             "argusToken": argus_token,
             "runData": {
                 "boonData": boon_data.strip(),
                 "weaponData": weapon_data.strip(),
                 "familiarData": familiar_data.strip(),
-                "extraData": extra_data.strip(),                        
-                "elementalData" : elemental_data.strip(),
-                "pinData" : pin_data.strip(),
+                "extraData": extra_data.strip(),
+                "elementalData": elemental_data.strip(),
+                "pinData": pin_data.strip(),
                 "vowData": vow_data.strip(),
-                "arcanaData": arcana_data.strip()
+                "arcanaData": arcana_data.strip(),
             },
-        })
+        },
+    )
     argus_log("Response: " + str(response))
+
 
 def read_data_from_stdin():
     boon_data = sys.stdin.readline()
@@ -57,10 +70,20 @@ def read_data_from_stdin():
     vow_data = sys.stdin.readline()
     arcana_data = sys.stdin.readline()
 
-    return boon_data, weapon_data, familiar_data, extra_data, elemental_data, pin_data, vow_data, arcana_data
+    return (
+        boon_data,
+        weapon_data,
+        familiar_data,
+        extra_data,
+        elemental_data,
+        pin_data,
+        vow_data,
+        arcana_data,
+    )
+
 
 def main():
-    if args["login"]: # doing just argus login and saving to config. no data sending
+    if args["login"]:  # doing just argus login and saving to config. no data sending
         argus_log("Doing argus login...")
         res = argus_auth.initial_auth_check(args["pluginpath"], argus_backend)
         if res == "HAVE_TOKEN":
@@ -70,13 +93,43 @@ def main():
         else:
             argus_log("Successful login with token: " + res)
     else:
-        boon_data, weapon_data, familiar_data, extra_data, elemental_data, pin_data, vow_data, arcana_data = read_data_from_stdin()
-        asyncio.run(send_to_backend(args["pluginpath"], boon_data, weapon_data, familiar_data, extra_data, elemental_data, pin_data, vow_data, arcana_data))
+        (
+            boon_data,
+            weapon_data,
+            familiar_data,
+            extra_data,
+            elemental_data,
+            pin_data,
+            vow_data,
+            arcana_data,
+        ) = read_data_from_stdin()
+        asyncio.run(
+            send_to_backend(
+                args["pluginpath"],
+                boon_data,
+                weapon_data,
+                familiar_data,
+                extra_data,
+                elemental_data,
+                pin_data,
+                vow_data,
+                arcana_data,
+            )
+        )
+
 
 if __name__ == "__main__":
-    arg_parser = argparse.ArgumentParser(description="Send Argus data to the Argus backend.")
-    arg_parser.add_argument('--pluginpath', required=True, help="Path to the folder in which files should be stored (normally, RoM's plugins_data).")
-    arg_parser.add_argument('--login', action="store_true", help="Just do login. No sending.")
+    arg_parser = argparse.ArgumentParser(
+        description="Send Argus data to the Argus backend."
+    )
+    arg_parser.add_argument(
+        "--pluginpath",
+        required=True,
+        help="Path to the folder in which files should be stored (normally, RoM's plugins_data).",
+    )
+    arg_parser.add_argument(
+        "--login", action="store_true", help="Just do login. No sending."
+    )
 
     args = vars(arg_parser.parse_args())
 
--- test/argus_testing.py
+++ test/argus_testing.py
@@ -1,4 +1,3 @@
-
 import configparser
 import boto3
 import json
@@ -9,7 +8,7 @@ import sys
 import os
 
 test_dir = os.path.dirname(os.path.abspath(__file__)) + "/"
-sys.path.insert(1, test_dir + '../src/py')
+sys.path.insert(1, test_dir + "../src/py")
 
 import send_to_argus
 import argus_test_generators
@@ -23,11 +22,12 @@ NOPINS = "NOPINS"
 NOVOWS = "NOVOWS"
 NOARCANA = "NOARCANA"
 
-# BUCKET_NAME = "argus-h2-backend-argus-tokens"
-BUCKET_NAME = "argus-h2-backend-argus-tokens-test"
+BUCKET_NAME = "argus-h2-backend-argus-tokens"
+# BUCKET_NAME = "argus-h2-backend-argus-tokens-test"
 UI_MAPPINGS_KEY = "uiMappings"
 
 GENERATOR_CONFIG_FILE = test_dir + "generator_params.ini"
+
 
 # This uses AWS data so we have a single source of truth.
 # If you want to run the test yourself,
@@ -36,32 +36,57 @@ GENERATOR_CONFIG_FILE = test_dir + "generator_params.ini"
 # and change this function to use that file instead
 def generate_test_data():
     s3_client = boto3.client("s3")
-    response = s3_client.get_object(
-                Bucket=BUCKET_NAME,
-                Key=UI_MAPPINGS_KEY
-            )
-    ui_mappings_string = response['Body'].read().decode('utf-8')
+    response = s3_client.get_object(Bucket=BUCKET_NAME, Key=UI_MAPPINGS_KEY)
+    ui_mappings_string = response["Body"].read().decode("utf-8")
     ui_mappings = json.loads(ui_mappings_string)
 
     generator_params = configparser.ConfigParser()
     generator_params.read(GENERATOR_CONFIG_FILE)
 
-    boon_data = argus_test_generators.boon_data_generator(ui_mappings, generator_params["boons"])
-    weapon_data = argus_test_generators.weapon_data_generator(ui_mappings, generator_params["weapons"])
-    familiar_data = argus_test_generators.familiar_data_generator(ui_mappings, generator_params["familiars"])
-    extra_data = argus_test_generators.keepsake_data_generator(ui_mappings, generator_params["keepsakes"])
-    elemental_data = argus_test_generators.elemental_data_generator(ui_mappings, generator_params["elements"])
-    pin_data = argus_test_generators.pin_data_generator(ui_mappings, generator_params["pins"])
-    vow_data = argus_test_generators.vow_data_generator(ui_mappings, generator_params["vows"])
-    arcana_data = argus_test_generators.arcana_data_generator(ui_mappings, generator_params["arcana"])
+    boon_data = argus_test_generators.boon_data_generator(
+        ui_mappings, generator_params["boons"]
+    )
+    weapon_data = argus_test_generators.weapon_data_generator(
+        ui_mappings, generator_params["weapons"]
+    )
+    familiar_data = argus_test_generators.familiar_data_generator(
+        ui_mappings, generator_params["familiars"]
+    )
+    extra_data = argus_test_generators.keepsake_data_generator(
+        ui_mappings, generator_params["keepsakes"]
+    )
+    elemental_data = argus_test_generators.elemental_data_generator(
+        ui_mappings, generator_params["elements"]
+    )
+    pin_data = argus_test_generators.pin_data_generator(
+        ui_mappings, generator_params["pins"]
+    )
+    vow_data = argus_test_generators.vow_data_generator(
+        ui_mappings, generator_params["vows"]
+    )
+    arcana_data = argus_test_generators.arcana_data_generator(
+        ui_mappings, generator_params["arcana"]
+    )
 
-    return boon_data, weapon_data, familiar_data, extra_data, elemental_data, pin_data, vow_data, arcana_data
+    return (
+        boon_data,
+        weapon_data,
+        familiar_data,
+        extra_data,
+        elemental_data,
+        pin_data,
+        vow_data,
+        arcana_data,
+    )
+
 
 def get_test_data(which_test):
     if which_test == "nominal":
         boon_data = "Common;;ApolloManaBoon Rare;;AphroditeSpecialBoon Epic;;PoseidonWeaponBoon Heroic;;PoseidonCastBoon Common;;DaggerBlinkAoETrait Duo;;PoseidonSplashSprintBoon Rare;;DoubleRewardBoon Legendary;;AmplifyConeBoon Infusion;;ElementalDodgeBoon"
         weapon_data = "Rare;;DaggerBlockAspect"
-        familiar_data = "8;;LastStandFamiliar 2;;LastStandFamiliar 3;;FamiliarCatResourceBonus"
+        familiar_data = (
+            "8;;LastStandFamiliar 2;;LastStandFamiliar 3;;FamiliarCatResourceBonus"
+        )
         extra_data = "Epic;;ForceZeusBoonKeepsake Common;;SpellSummonTrait"
         elemental_data = "Fire:1;;Water:0;;Earth:3;;Air:0;;Aether:0"
         pin_data = "RandomStatusBoon;;AresExCastBoon;;GoodStuffBoon"
@@ -79,7 +104,9 @@ def get_test_data(which_test):
     elif which_test == "doubleboon":
         boon_data = "Common;;ApolloManaBoon Rare;;AphroditeSpecialBoon Epic;;PoseidonWeaponBoon Heroic;;PoseidonCastBoon Common;;DaggerBlinkAoETrait Duo;;PoseidonSplashSprintBoon Rare;;DoubleRewardBoon Legendary;;AmplifyConeBoon Infusion;;ElementalDodgeBoon Duo;;PoseidonSplashSprintBoon"
         weapon_data = "Rare;;DaggerBlockAspect"
-        familiar_data = "8;;LastStandFamiliar 2;;LastStandFamiliar 3;;FamiliarCatResourceBonus"
+        familiar_data = (
+            "8;;LastStandFamiliar 2;;LastStandFamiliar 3;;FamiliarCatResourceBonus"
+        )
         extra_data = "Epic;;ForceZeusBoonKeepsake Common;;SpellSummonTrait"
         elemental_data = "Fire:1;;Water:0;;Earth:3;;Air:0;;Aether:0"
         pin_data = "RandomStatusBoon;;AresExCastBoon;;GoodStuffBoon"
@@ -88,7 +115,9 @@ def get_test_data(which_test):
     elif which_test == "pins":
         boon_data = "Common;;ApolloManaBoon Rare;;AphroditeSpecialBoon Epic;;PoseidonWeaponBoon Heroic;;PoseidonCastBoon Common;;DaggerBlinkAoETrait Duo;;PoseidonSplashSprintBoon Rare;;DoubleRewardBoon Legendary;;AmplifyConeBoon Infusion;;ElementalDodgeBoon"
         weapon_data = "Rare;;DaggerBlockAspect"
-        familiar_data = "8;;LastStandFamiliar 2;;LastStandFamiliar 3;;FamiliarCatResourceBonus"
+        familiar_data = (
+            "8;;LastStandFamiliar 2;;LastStandFamiliar 3;;FamiliarCatResourceBonus"
+        )
         extra_data = "Epic;;ForceZeusBoonKeepsake Common;;SpellSummonTrait"
         elemental_data = "Fire:1;;Water:0;;Earth:3;;Air:0;;Aether:0"
         pin_data = "LuckyBoon;;ElementalUnifiedBoon;;GoodStuffBoon"
@@ -103,21 +132,70 @@ def get_test_data(which_test):
         pin_data = NOPINS
         vow_data = NOVOWS
         arcana_data = NOARCANA
-    return boon_data, weapon_data, familiar_data, extra_data, elemental_data, pin_data, vow_data, arcana_data
+    return (
+        boon_data,
+        weapon_data,
+        familiar_data,
+        extra_data,
+        elemental_data,
+        pin_data,
+        vow_data,
+        arcana_data,
+    )
 
 
 def main():
     if args["generate"]:
-        boon_data, weapon_data, familiar_data, extra_data, elemental_data, pin_data, vow_data, arcana_data = generate_test_data()
+        (
+            boon_data,
+            weapon_data,
+            familiar_data,
+            extra_data,
+            elemental_data,
+            pin_data,
+            vow_data,
+            arcana_data,
+        ) = generate_test_data()
     else:
-        boon_data, weapon_data, familiar_data, extra_data, elemental_data, pin_data, vow_data, arcana_data = get_test_data(args['prepared'])
-    asyncio.run(send_to_argus.send_to_backend(test_dir, boon_data, weapon_data, familiar_data, extra_data, elemental_data, pin_data, vow_data, arcana_data))
+        (
+            boon_data,
+            weapon_data,
+            familiar_data,
+            extra_data,
+            elemental_data,
+            pin_data,
+            vow_data,
+            arcana_data,
+        ) = get_test_data(args["prepared"])
+    asyncio.run(
+        send_to_argus.send_to_backend(
+            test_dir,
+            boon_data,
+            weapon_data,
+            familiar_data,
+            extra_data,
+            elemental_data,
+            pin_data,
+            vow_data,
+            arcana_data,
+        )
+    )
+
 
 if __name__ == "__main__":
-    
-    arg_parser = argparse.ArgumentParser(description="Test the backend and frontend with prepared or generated data.")
-    arg_parser.add_argument('--generate', action="store_true", help="Generate a random test based on parameters.")
-    arg_parser.add_argument('--prepared', choices=["nominal", "minimal", "doubleboon", "pins"], help="Run one of the prepared tests.")
+    arg_parser = argparse.ArgumentParser(
+        description="Test the backend and frontend with prepared or generated data."
+    )
+    arg_parser.add_argument(
+        "--generate",
+        action="store_true",
+        help="Generate a random test based on parameters.",
+    )
+    arg_parser.add_argument(
+        "--prepared",
+        choices=["nominal", "minimal", "doubleboon", "pins"],
+        help="Run one of the prepared tests.",
+    )
 
     args = vars(arg_parser.parse_args())
     main()
